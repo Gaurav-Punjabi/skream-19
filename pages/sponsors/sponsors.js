01fe0ce1ad10e97cbeb535f34a02c3a6b7cbff9a
Gaurav Punjabi <gaurav.punjabi6@icloud.com>
2018-12-14 20:38:00 +0530

    Fixed Sponsors Page:

--- pages/sponsors/sponsors.js
+++ pages/sponsors/sponsors.js
@@ -1,30 +1,42 @@
-(
-    function($) {
-        var NavbarHandler= { 
-            init: function() {
-                this.cacheDom();
-                this.eventBind()
-            }, 
-            cacheDom: function() {
-                this.container = $("#container"); 
-                this.mainPage = $("#main");
-                this.sideBar = $(".side-bar");
-                this.toggleButton = $("#fuck");
-                this.mainContainer = $("#main .container");
-                this.window = $(window);
-                this.usesVH = $(".usesvh");
-                this.usesVH2=$("div")
-            },
-            toggleNav: function() {
-                console.log("helloWorld");
-                this.container.toggleClass("openbar1");
-                this.mainPage.toggleClass("openbar");
-                this.sideBar.toggleClass("openbar2")
-            }, 
-            eventBind: function() {
-                this.toggleButton.on("click",this.toggleNav.bind(this))
-            }
-        };
-        NavbarHandler.init()
-    })(jQuery);
-    $("#menu-toggle").click(function(e){e.preventDefault();$("#wrapper").toggleClass("toggled")});
+$(function () {
+    hideAll();
+    $('#2018').show();
+    $('#2019-pill').click(function () {
+        hideAll();
+        $('#2019').show();
+        $('#2019-pill').addClass("active");
+        $('#2018-pill').removeClass("active");
+        $('#2017-pill').removeClass("active");
+        $('#2016-pill').removeClass("active");
+    });
+    $('#2018-pill').click(function () {
+        hideAll();
+        $('#2018').show();
+        $('#2018-pill').addClass("active");
+        $('#2017-pill').removeClass("active");
+        $('#2019-pill').removeClass("active");
+        $('#2016-pill').removeClass("active");
+    });
+    $('#2017-pill').click(function () {
+        hideAll();
+        $('#2017').show();
+        $('#2017-pill').addClass("active");
+        $('#2018-pill').removeClass("active");
+        $('#2019-pill').removeClass("active");
+        $('#2016-pill').removeClass("active");
+    });
+    $('#2016-pill').click(function () {
+        hideAll();
+        $('#2016').show();
+        $('#2016-pill').addClass("active");
+        $('#2017-pill').removeClass("active");
+        $('#2019-pill').removeClass("active");
+        $('#2018-pill').removeClass("active");
+    });
+    function hideAll() {
+        $('#2019').hide();
+        $('#2018').hide();
+        $('#2017').hide();
+        $('#2016').hide();
+    }
+});
