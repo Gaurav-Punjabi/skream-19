a11d95d308afef0371ca42566fcf478ee3a9170e
harmitsampat96 <harmit.s@somaiya.edu>
2019-12-17 20:37:56 +0530

    Changes done

--- trash/index.js
+++ trash/index.js
@@ -6,11 +6,18 @@ $(function() {
             $('.sidebar-wrapper').addClass('close');
             $('.section').removeClass('sidebar-open');
             $('.section').addClass('sidebar-close');
+//            document.body.style.background = "#f3f3f3 url('img_tree.png') no-repeat right top";
+//            $('.open-img').style.display=none;
+//            $('.close-img').style.display=block;
+//            
         } else {
             $('.sidebar-wrapper').addClass('open');
             $('.sidebar-wrapper').removeClass('close');
             $('.section').removeClass('sidebar-close');
             $('.section').addClass('sidebar-open');
+            
+//            $('.open-img').style.display=block;
+//            $('.close-img').style.display=none;
         }
         sidebarOpen = !sidebarOpen;
     });
