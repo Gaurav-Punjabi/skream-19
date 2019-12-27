27ba2bc25a4d450e0f07d5f8fb06401a0782ba81
Isha Joglekar <ijoglekar16@gmail.com>
2019-12-27 17:44:34 +0530

    added 2020 sponsors

--- pages/sponsors/sponsors.js
+++ pages/sponsors/sponsors.js
@@ -4,16 +4,16 @@ $(function () {
 
 
     $('#2019').show();
-    //    $('#2020-pill').click(function () {
-    //        hideAll();
-    //        console.log("Clicked 2020");
-    //        $('#2020').show();
-    //        $('#2020-pill').addClass("active");
-    //        $('#2019-pill').removeClass("active");
-    //        $('#2018-pill').removeClass("active");
-    //        $('#2017-pill').removeClass("active");
-    //        $('#2016-pill').removeClass("active");
-    //    });
+        $('#2020-pill').click(function () {
+            hideAll();
+            console.log("Clicked 2020");
+            $('#2020').show();
+            $('#2020-pill').addClass("active");
+            $('#2019-pill').removeClass("active");
+            $('#2018-pill').removeClass("active");
+            $('#2017-pill').removeClass("active");
+            $('#2016-pill').removeClass("active");
+        });
 
 
     $('#2019-pill').click(function () {
